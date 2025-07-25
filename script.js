4950081c2aa526c257f63bfa19fc42e283c581da
David_Arruda <david.viniciusarruda@gmail.com>
2025-07-25 17:02:27 -0300

    Adiciona verificação das entradas

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const exit = document.querySelector("#exit-input")
 const sortearBotao = document.querySelector("button")
 
-function generateNumber(){
+function generateNumber() {
 
     const min = Math.ceil(document.querySelector("#min-input").value)
     const max = Math.floor(document.querySelector("#max-input").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    exit.value = result
+    if (min < max) {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        exit.value = result
+    } else {
+        alert("Erro no intervalo: O primeiro número deve ser menor ao segundo.")
+    }
 }
 
 sortearBotao.addEventListener("click", generateNumber)
